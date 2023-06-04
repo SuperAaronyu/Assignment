de71a7ada4806fc09228c6ced774c6305195a819
AY <yu@LUN>
2023-06-04 23:56:55 +0930

    as

---
+++ question18.sh
@@ -0,0 +1,22 @@
+cd git-practice-04
+git checkout ready1
+git checkout ready2
+git checkout ready3
+git checkout main
+git merge ready1
+git merge ready2
+git merge ready3
+git add -A
+git commit -m 'a'
+git checkout update1
+git branch -D ready1
+git branch -D ready2
+git branch -D ready3
+git merge main
+git add -A
+git commit -m 's'
+git checkout update2
+git merge main 
+git add -A
+git commit -m 's'
+
