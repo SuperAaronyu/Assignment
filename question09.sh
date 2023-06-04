8793daf717d21dddee2e98656458b23a34845dee
AY <yu@LUN>
2023-06-04 22:47:45 +0930

    s

--- question09.sh
+++
@@ -1,5 +0,0 @@
-git branch branch1
-git checkout branch1
-touch file3
-git add -A
-git commit -m 'all'
--- question14.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/ian-knight-uofa/git-practice-04.git
-cd git-practice-04
-git branch update1
